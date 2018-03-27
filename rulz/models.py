97bda8043bf42513842c74157929839bea8a0a02
MBoidot <mathieu.boidot@gmail.com>
2018-03-27 18:36:22 -0400

    building model

--- rulz/models.py
+++ rulz/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Rulz(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    country = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    player_num = models.IntegerField()
+    complexity = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
-    author = models.TextField()
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
