0b000ccf00ef7edd63827e4a3af8a4faac571ebf
MBoidot <mathieu.boidot@gmail.com>
2018-03-29 19:26:33 -0400

    setting detail view

--- rulz/urls.py
+++ rulz/urls.py
@@ -7,10 +7,9 @@ app_name = 'rulz'
 urlpatterns = [
     url(r'^', views.rules_home, name='rulz_home'),
     #rulz/index
-    url(r'index', views.IndexView.as_view(), name='rulz_index'),
-    #rulz/number
-
-
+    url(r'index', views.Rulz_IndexView.as_view(), name='rulz_index'),
+    #rulz/ i.e. rulz/number
+    url(r'^(?P<rulz_id>[0-9]+)$',views.Rulz_DetailView.as_view(),name='rulz_detail')
 
     url(r'create/$', views.RulzCreate.as_view(), name='rulz_create'),
 ]
--- rulz/views.py
+++ rulz/views.py
@@ -8,12 +8,16 @@ def rules_home(request):
     template_name = "rulz/home_template.html"
     return render(request,template_name)
 
-class IndexView(generic.ListView):
+class Rulz_IndexView(generic.ListView):
     template_name = "rulz/index_template.html"
     context_object_name = 'all_rulz'
 
     def get_queryset(self):
         return Rulz.objects.all()
+
+class Rulz_DetailView(generic.DetailView):
+    template_name = "rulz/detail_template.html"
+    #have to pass stuff to the request like Id of the current rule etc..., Check also on the button that leads here on the template (index_tempalte in rules)
 
 
 class RulzCreate(CreateView):
