10aad620a252c8db96a3e789fe432164a85e5d95
MBoidot <mathieu.boidot@gmail.com>
2018-04-18 22:53:52 -0400

    login implementation

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
+
 from . import views
+from django.contrib.auth.views import login, logout
 
 
 app_name = 'home'
 
 urlpatterns = [
+    url(r'^login/$', login, {'template_name': 'home/login.html'},name='login'),
+    url(r'^logout/$', logout, name='logout'),
     url(r'^register/$', views.UserFormView.as_view(), name='register'),
     url(r'^', views.HomeView, name='home'),
 ]
